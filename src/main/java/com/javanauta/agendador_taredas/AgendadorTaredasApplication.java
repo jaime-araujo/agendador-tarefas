9cec632547f4d57f46f70d3b7cf60c37f2a7a827
Jaime <jaimehraraujo@gmail.com>
2026-06-10 16:06:06 -0300

    commit incial com esqueleto

---
+++ src/main/java/com/javanauta/agendador_taredas/AgendadorTaredasApplication.java
@@ -0,0 +1,13 @@
+package com.javanauta.agendador_taredas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendadorTaredasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendadorTaredasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javanauta/agendador_taredas/AgendadorTaredasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javanauta.agendador_taredas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorTaredasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
